e6dd8134868f278fb8ff493134306f76a48a0fac
Andrew Argeros <aargeros15@gmail.com>
2024-10-15 12:08:33 -0500

    fix: download button

--- pages/2_Create_A_QR_Code.py
+++ pages/2_Create_A_QR_Code.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pandas as pd
 import segno
+import os
 from PIL import Image
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
+from io import BytesIO
+from datetime import datetime
 
 JLM_LOGO = Image.open(".streamlit/jlm-logo.png")
 
@@ -28,7 +31,7 @@ def format_qr(qr_image_path):
 
     qr_width, qr_height = qr_image.size
     logo_size = min(qr_width, qr_height) // 4
-    logo = logo.resize((logo_size, logo_size), Image.ANTIALIAS)
+    logo = logo.resize((logo_size, logo_size), Image.LANCZOS)
 
     logo_x = (qr_width - logo_size) // 2
     logo_y = (qr_height - logo_size) // 2
@@ -39,10 +42,17 @@ def format_qr(qr_image_path):
 
 
 def save_all_qr_codes(df):
+    temp_dir = f"media/{datetime.now().strftime('%Y%m%d%H%M%S')}"
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
     for row in df.itertuples():
-        qr_image_path = create_qr(row.link, f"media/{row.title.split('.')[0]}.png")
+        qr_image_path = create_qr(row.link, f"{temp_dir}/{row.title.split('.')[0]}.png")
         qr = format_qr(qr_image_path)
-        qr.save(f"media/{row.title.split('.')[0]}.png")
+        qr.save(f"{temp_dir}/{row.title.split('.')[0]}.png")
+
+    os.system(f"zip -r {temp_dir}.zip {temp_dir}")
+    os.system(f"rm -rf {temp_dir}")
+    return temp_dir + ".zip"
 
 
 def load_drive_csv(drive):
@@ -52,9 +62,16 @@ def load_drive_csv(drive):
     return pd.read_csv("all_mp3_files.csv")
 
 
+def create_button():
+    st.session_state["create"] = True
+
+
 if "df" not in st.session_state:
     drive = create_drive()
     st.session_state.df = load_drive_csv(drive)
+
+if "create" not in st.session_state:
+    st.session_state.create = False
 
 st.set_page_config(page_title="Create a QR Code", page_icon=JLM_LOGO)
 
@@ -77,12 +94,14 @@ choice = st.selectbox(
 
 create1, create2 = st.columns(2)
 with create1:
-    if st.button(
+    st.button(
         "Create QR Code",
         type="primary",
         icon=":material/qr_code_2_add:",
         use_container_width=True,
-    ):
+        on_click=create_button,
+    )
+    if st.session_state["create"]:
         url = (
             st.session_state["df"]
             .loc[st.session_state["df"]["title"] == choice, "link"]
@@ -91,16 +110,20 @@ with create1:
         qr_image_path = create_qr(url)
         qr_image = format_qr(qr_image_path)
 
-        img_c1, img_c2 = st.columns(2)
+        st.image(qr_image.resize((6 * 300, 6 * 300)), width=300)
 
-        with img_c1:
-            st.image(qr_image.resize((6 * 300, 6 * 300)), width=300)
+        file_name = st.text_input("Enter the file name", value="qr.png")
+        qr_image_buffer = BytesIO()
+        qr_image.save(qr_image_buffer, format="PNG")
+        st.download_button(
+            "Download QR Code",
+            qr_image_buffer.getvalue(),
+            file_name,
+            icon=":material/download:",
+            mime="image/png",
+            use_container_width=True,
+        )
 
-        with img_c2:
-            file_name = st.text_input("Enter the file name", value="qr.png")
-            st.button(
-                "Download QR Code", qr_image.save, file_name, icon=":material/download:"
-            )
 with create2:
     if st.button(
         "Save All QR Codes",
@@ -108,6 +131,13 @@ with create2:
         icon=":material/downloading:",
         use_container_width=True,
     ):
-        save_all_qr_codes(st.session_state["df"])
-        st.write("All QR codes saved to media folder")
+        zip_file_path = save_all_qr_codes(st.session_state["df"])
+        with open(zip_file_path, "rb") as fp:
+            st.download_button(
+                label="Download ZIP with all QR Codes",
+                data=fp,
+                file_name=zip_file_path.split("/")[-1],
+                mime="application/zip",
+                icon=":material/download:",
+            )
         st.balloons()
